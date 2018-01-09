aacb0f716c7906f61737cb2cddbc950433673401
Kudryatzhan Arziyev <karziyev@gmail.com>
2018-01-09 10:05:00 -0700

    Rearranged project folders, Ship and Settings model created

---
+++ BattleShipGame/BattleShipGame/Model/Ship.swift
@@ -0,0 +1,16 @@
+//
+//  Ship.swift
+//  BattleShipGame
+//
+//  Created by Kudryatzhan Arziyev on 1/9/18.
+//  Copyright © 2018 Kudryatzhan Arziyev. All rights reserved.
+//
+
+import Foundation
+
+struct Ship {
+    let length: Int
+    var isHit = false
+    var isDestoyed = false
+    var hitNumber = 0
+}
